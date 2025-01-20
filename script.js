7c1f4467e5a3b32ee17ddb19363eb92d0373b638
gnaneshwardammu <161947558+gnaneshwardammu@users.noreply.github.com>
2025-01-20 23:06:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const qrText = document.getElementById('qr-text');
+const sizes = document.getElementById('sizes');
+const generateBtn = document.getElementById('generateBtn');
+const downloadBtn = document.getElementById('downloadBtn');
+
+const qrContainer = document.querySelector('.qr-body');
+let size = sizes.value;
+generateBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    isEmptyInput();
+});
+
+sizes.addEventListener('change',(e)=>{
+    size = e.target.value;
+    isEmptyInput();
+});
+
+downloadBtn.addEventListener('click',()=>{
+    let img = document.querySelector('.qr-body img');
+    if(img !== null){
+        let imgAtrr = img.getAttribute('src');
+        downloadBtn.setAttribute("href", imgAtrr);
+    }
+    else{
+        downloadBtn.setAttribute("href", `${document.querySelector('canvas').toDataUrl()}`);
+    }
+})
+function isEmptyInput(){
+    if(qrText.value.length > 0){
+        genarateQRCode();
+    }
+    else{
+        alert("Enter the text or URL to generate your QR code");
+    }
+}
+
+function genarateQRCode(){
+    qrContainer.innerHTML = "";
+    new QRCode(qrContainer, {
+        text:qrText.value,
+        height:size,
+        width:size,
+        colorLight:"#fff",
+        colorDark:"#000",
+    })
+}
